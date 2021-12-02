050abe1690dd7af00fdd102ce94bb3e104f7cdbf
William Menghi Hirano <whirano4@gmail.com>
2021-12-02 16:51:35 -0300

    Criei pag-redes, title, desc e favicon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PagRedesComponent } from './view/pag-redes/pag-redes.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagRedesComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/view/pag-redes/pag-redes.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pag-redes',
+  templateUrl: './pag-redes.component.html',
+  styleUrls: ['./pag-redes.component.scss']
+})
+export class PagRedesComponent{}
