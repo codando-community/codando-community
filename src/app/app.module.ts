f6d285e413736e5f6bf13ffcab88e4efb19444a0
Gabriel Murta De Freitas <gabrielmurta7@gmail.com>
2021-12-03 18:08:43 -0300

    Adicionando a NavBar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NetworkComponent } from './view/network/network.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NetworkComponent
+    NetworkComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.scss']
+})
+export class NavBarComponent  {
+
+}
