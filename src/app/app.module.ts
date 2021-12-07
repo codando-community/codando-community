563804e81f85b482f2b503bc913dc714224e8315
Gabriel Murta De Freitas <gabrielmurta7@gmail.com>
2021-12-07 14:25:31 -0300

    Adicionando o modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NetworkComponent } from './view/network/network.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { ModalComponent } from './components/modal/modal.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NetworkComponent,
-    NavbarComponent
+    NavbarComponent,
+    ModalComponent,
+    
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/modal/modal.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss']
+})
+export class ModalComponent {
+  mostrar: boolean = true;
+
+  toggle () {
+    this.mostrar = !this.mostrar;
+  }
+}
