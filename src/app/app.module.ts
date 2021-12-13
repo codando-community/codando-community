906262c8be08cda930ffe372efdd9a1bb3db8b85
William Menghi Hirano <whirano4@gmail.com>
2021-12-13 16:09:25 -0300

    Fazendo página de pilares

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { NetworkComponent } from './view/network/network.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ModalComponent } from './components/modal/modal.component';
+import { PrinciplesComponent } from './view/principles/principles.component';
 
 
 
@@ -14,6 +15,7 @@ import { ModalComponent } from './components/modal/modal.component';
     NetworkComponent,
     NavbarComponent,
     ModalComponent,
+    PrinciplesComponent,
     
   ],
   imports: [
