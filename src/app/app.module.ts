f576be8b942049c89336d3119fb8859431b78cfa
William Menghi Hirano <whirano4@gmail.com>
2022-03-13 21:42:12 -0300

    Revert "Issue#61 - Pág. Events"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NetworkComponent } from './view/network/network.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ModalComponent } from './components/modal/modal.component';
-import { NetworkComponent } from './view/network/network.component';
-import { AboutComponent } from './view/about/about.component';
 import { PrinciplesComponent } from './view/principles/principles.component';
+import { AboutComponent } from './view/about/about.component';
 import { CodyComponent } from './view/cody/cody.component';
-import { EventsComponent } from './view/events/events.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    NetworkComponent,
     NavbarComponent,
     ModalComponent,
-    NetworkComponent,
-    AboutComponent,
     PrinciplesComponent,
+    AboutComponent,
     CodyComponent,
-    EventsComponent,
+    
   ],
   imports: [
     BrowserModule
